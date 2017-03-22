1ae8518e74a5caf0fae6846e5d9773d72ef1b205
klone1127 <falcon_sky@yeah.net>
2017-03-23 00:05:48 +0800

    generic views

--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,26 @@
-from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-
-from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
+from django.http import HttpResponseRedirect
 
 from .models import Choice, Question
 
 # Create your views here.
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    def get_queryset(self):
+        """Return the last five published questions. """
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -31,7 +33,7 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.vote += 1
+        selected_choice.votes += 1
         selected_choice.save()
         # Always return an HttpResposeRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
